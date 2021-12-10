edc015a738abd4910e4a63405f6366ccd661ceaf
justinrivera47 <justinrivera47@gmail.com>
2021-12-10 12:12:03 -0500

    finsihed with react.

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,5 +1,27 @@
 // Write your Character component here
 import React from 'react'
+import styled from 'styled-components'
+
+const CardBody = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+  padding: 4px;
+  border-bottom: 2px solid;
+  background-color: linear-gradient(rgba(0, 250, 0, 0.9));
+  border-radius: 4px;
+  margin-left: 190px;
+  color: black;
+
+  transition: all 0.9s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: white;
+  }
+  
+ `
+
+
 
 const Characters = (props) => {
 
@@ -7,17 +29,18 @@ console.log(props)
 const {data} = props
 
   return (
-    <section>
-      <h1>Characters</h1>
+    <div>
+      <h1 className="Header">REACT WARS</h1>
       {
         data.map((characters) => (
-          <article key={characters.name}>
+          <CardBody key={characters.name}>
             <h2>{characters.name}</h2>
+            <p>{characters.hair_color}</p>
             
-          </article>
+          </CardBody>
         ))
       }
-    </section>
+    </div>
   )
 }
 
